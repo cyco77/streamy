11196f752b12c861d4432f31857ea7f5dbe5d858
Lars Hildebrandt <info@larshildebrandt.de>
2017-01-01 17:39:44 +0100

    structure changed

--- mod_streamy/tmpl/default.php
+++
@@ -1,8 +0,0 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); 
-
-$template = str_replace('{name}',$channel->url,$channel->urltemplate);
-$template = str_replace('{width}',$width,$template);
-$template = str_replace('{height}',$height,$template);
-
-echo $template;
